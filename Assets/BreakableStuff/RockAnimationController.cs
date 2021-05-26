a2e837c3435b0fc552dda1e00c8f4528ce760da9
Draaek <69803716+Draaek@users.noreply.github.com>
2021-05-26 11:40:03 -0500

    Added Rewindable rock & code

---
+++ Assets/BreakableStuff/RockAnimationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockAnimationController : MonoBehaviour
+{
+
+    Animator rockAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rockAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RebuildTrigger()
+    {
+        rockAnimator.SetTrigger("RebuildRock");
+    }
+}
